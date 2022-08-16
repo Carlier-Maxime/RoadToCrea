668b415ab567688b9dcf362d3a9149c90a473737
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-08-16 18:15:06 +0200

    add void dimension

--- src/main/java/fr/metouais/roadtocrea/RoadToCrea.java
+++ src/main/java/fr/metouais/roadtocrea/RoadToCrea.java
@@ -2,15 +2,26 @@ package fr.metouais.roadtocrea;
 
 import fr.metouais.roadtocrea.init.ModBlocks;
 import fr.metouais.roadtocrea.init.ModItems;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
 
 @Mod(RoadToCrea.MODID)
 public class RoadToCrea {
     public static final String MODID = "roadtocrea";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final ResourceLocation DIM_ID = new ResourceLocation(MODID, "void");
 
     public RoadToCrea() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -28,5 +39,9 @@ public class RoadToCrea {
 
     private void clientSetup(FMLClientSetupEvent e) {
 
+    }
+
+    public static Optional<Level> getVoidWorld(MinecraftServer server) {
+        return Optional.ofNullable(server.getLevel(ResourceKey.create(Registry.DIMENSION_REGISTRY, DIM_ID)));
     }
 }
