684cc67ec8069266fad07fa82cd18fa291b95a80
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-08-17 19:38:51 +0200

    ultimate bedrock emit light (15)

--- src/main/java/fr/metouais/roadtocrea/init/ModBlocks.java
+++ src/main/java/fr/metouais/roadtocrea/init/ModBlocks.java
@@ -15,7 +15,7 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, RoadToCrea.MODID);
 
-    public static final RegistryObject<Block> ULTIMATE_BEDROCK = createBlock("ultimate_bedrock", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).destroyTime(-1f).speedFactor(0.001f)));
+    public static final RegistryObject<Block> ULTIMATE_BEDROCK = createBlock("ultimate_bedrock", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).destroyTime(-1f).speedFactor(0.001f).lightLevel((param) -> 15)));
 
     public static RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier) {
         RegistryObject<Block> block = BLOCKS.register(name, supplier);
