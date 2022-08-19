d10ce7077661ba91bd43cc22514ec6ab30255e6d
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-08-19 04:49:02 +0200

    add knife for get raw human meat

--- src/main/java/fr/metouais/roadtocrea/init/ModItems.java
+++ src/main/java/fr/metouais/roadtocrea/init/ModItems.java
@@ -1,14 +1,12 @@
 package fr.metouais.roadtocrea.init;
 
 import fr.metouais.roadtocrea.RoadToCrea;
-import net.minecraft.world.item.BlockItem;
+import fr.metouais.roadtocrea.item.KnifeItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
-import java.util.function.Supplier;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RoadToCrea.MODID);
@@ -16,4 +14,6 @@ public class ModItems {
     public static final RegistryObject<Item> TINY_COBBLESTONE_PEBBLE = ITEMS.register("tiny_cobblestone_pebble", () -> new Item(new Item.Properties().tab(RoadToCrea.ROADTOCREA_TAB)));
     public static final RegistryObject<Item> COBBLESTONE_PEBBLE = ITEMS.register("cobblestone_pebble", () -> new Item(new Item.Properties().tab(RoadToCrea.ROADTOCREA_TAB)));
     public static final RegistryObject<Item> BIG_COBBLESTONE_PEBBLE = ITEMS.register("big_cobblestone_pebble", () -> new Item(new Item.Properties().tab(RoadToCrea.ROADTOCREA_TAB)));
+    public static final RegistryObject<Item> COBBLESTONE_KNIFE = ITEMS.register("cobblestone_knife", () -> new KnifeItem(Tiers.WOOD,3,0.5f,new Item.Properties().tab(RoadToCrea.ROADTOCREA_TAB)));
+    public static final RegistryObject<Item> RAW_HUMAN_MEAT = ITEMS.register("raw_human_meat", () -> new Item(new Item.Properties().tab(RoadToCrea.ROADTOCREA_TAB)));
 }
---
+++ src/main/java/fr/metouais/roadtocrea/item/KnifeItem.java
@@ -0,0 +1,39 @@
+package fr.metouais.roadtocrea.item;
+
+import fr.metouais.roadtocrea.init.ModBlocks;
+import fr.metouais.roadtocrea.init.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+public class KnifeItem extends SwordItem {
+    public KnifeItem(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_) {
+        super(p_43269_, p_43270_, p_43271_, p_43272_);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext useOnContext) {
+        Player player = useOnContext.getPlayer();
+        if (player == null) return super.useOn(useOnContext);
+        Level level = useOnContext.getLevel();
+        if (player.isCrouching() && level.getBlockState(player.blockPosition().below()).getBlock().equals(ModBlocks.ULTIMATE_BEDROCK.get())) {
+            player.hurt(new DamageSource("cobblestone_knife"), this.getDamage()*3);
+            player.causeFoodExhaustion(3f);
+            player.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 1800));
+            player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 1800));
+            player.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 1800));
+            player.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 100));
+            BlockPos pos = player.blockPosition();
+            level.addFreshEntity(new ItemEntity(level, pos.getX(), pos.getY(), pos.getZ(), ModItems.RAW_HUMAN_MEAT.get().getDefaultInstance()));
+        }
+        return super.useOn(useOnContext);
+    }
+}
