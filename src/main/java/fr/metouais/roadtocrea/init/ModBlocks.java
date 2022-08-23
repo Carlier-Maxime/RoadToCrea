df3427508aae7a48b1c2872dac2951ee8d1d1143
M_EtOuais <28m06c02@gmail.com>
2022-08-24 00:06:58 +0200

    add moss slab
    Other Change :
    - add recipe moss carpet with moss ball
    - add recipe moss slab with moss carpet
    - add recipe moss block with moss slab

--- src/main/java/fr/metouais/roadtocrea/init/ModBlocks.java
+++ src/main/java/fr/metouais/roadtocrea/init/ModBlocks.java
@@ -6,8 +6,11 @@ import fr.metouais.roadtocrea.block.UltimateBedrockBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -18,7 +21,8 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, RoadToCrea.MODID);
 
     public static final RegistryObject<Block> ULTIMATE_BEDROCK = createBlock("ultimate_bedrock", () -> new UltimateBedrockBlock(BlockBehaviour.Properties.of(Material.STONE).destroyTime(-1f).speedFactor(0.001f).lightLevel((param) -> 15)));
-    public static final RegistryObject<Block> MANGROVE_CRAFTING_TABLE = createBlock("mangrove_crafting_table", () -> new CustomCraftingTableBlock(BlockBehaviour.Properties.of(Material.WOOD)));
+    public static final RegistryObject<Block> MANGROVE_CRAFTING_TABLE = createBlock("mangrove_crafting_table", () -> new CustomCraftingTableBlock(BlockBehaviour.Properties.of(Material.WOOD).strength(2.5F).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> MOSS_SLAB = createBlock("moss_slab", () -> new SlabBlock(BlockBehaviour.Properties.of(Material.MOSS, MaterialColor.COLOR_GREEN).strength(0.1F).sound(SoundType.MOSS)));
 
     public static RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier) {
         RegistryObject<Block> block = BLOCKS.register(name, supplier);
